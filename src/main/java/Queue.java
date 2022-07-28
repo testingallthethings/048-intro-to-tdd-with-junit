ecf7fd184fadf10cd5b99369fc77b137cbe6b4b8
Mark Bradley <braddle@gmail.com>
2022-07-28 10:21:51 +0100

    Implementing a Queue

--- src/main/java/Queue.java
+++ src/main/java/Queue.java
@@ -1,2 +1,25 @@
 public class Queue {
+
+    private int size = 0;
+    private String[] elements = new  String[10];
+    private int front = 0;
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void join(String value) {
+        elements[size] = value;
+        size++;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public String serve() {
+        size--;
+        String element = elements[front];
+        front++;
+        return element;
+    }
 }
--- src/test/java/QueueTest.java
+++ src/test/java/QueueTest.java
@@ -1,7 +1,69 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class QueueTest {
 
+    private Queue empty, one, two;
+
+    @BeforeEach
+    void setUp() {
+        empty = new Queue();
+
+        one = new Queue();
+        one.join("red");
+
+        two = new Queue();
+        two.join("red");
+        two.join("blue");
+    }
+
+    @Test
+    void emptyQueueIsEmpty() {
+        assertTrue(empty.isEmpty());
+    }
+
+    @Test
+    void nonEmptyQueueIsNotEmpty() {
+        assertFalse(one.isEmpty());
+    }
+
+    @Test
+    void emptyQueueSize() {
+        assertEquals(0, empty.size());
+    }
+
+    @Test
+    void oneQueueSize() {
+        assertEquals(1, one.size());
+    }
+
+    @Test
+    void twoQueueSize() {
+        assertEquals(2, two.size());
+    }
+
+    @Test
+    void oneQueueServe() {
+        assertEquals("red", one.serve());
+        assertEquals(0, one.size());
+        assertTrue(one.isEmpty());
+    }
+
+    @Test
+    void twoQueueServerFirst() {
+        assertEquals("red", two.serve());
+        assertEquals(1, two.size());
+        assertFalse(two.isEmpty());
+    }
+
+    @Test
+    void twoQueueServerSecond() {
+        two.serve();
+
+        assertEquals("blue", two.serve());
+        assertEquals(0, two.size());
+        assertTrue(two.isEmpty());
+    }
 }
